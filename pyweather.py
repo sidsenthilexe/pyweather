5c592ed055b50b11c0605e89f1d5f788cdd0e9af
Sid Senthil <sidsenthil2010@gmail.com>
2024-11-17 13:42:32 -0800

    Update pyweather.py
    -Started working on gui, doesn't work fully yet

--- pyweather.py
+++ pyweather.py
@@ -4,35 +4,61 @@ from datetime import datetime
 import os
 
 load_dotenv()
-
 apiKey=os.getenv('apiKey')
 
-userInputCity = input("Enter city: ")
-userInputSystem = input("Enter your preferred system of measurements (I/M): ")
-requestMeasurementSystem = "I"
+from tkinter import *
+root = Tk()
+root.geometry("400x400")
 
-if userInputSystem == "I":
+
+title = Label(root, text='pyweather').grid(row=0)
+ready = 0
+
+def readyPressed():
+    ready=1
+
+
+Label(root, text='City: ').grid(row=1)
+userInputCity = Entry(root)
+userInputCity.grid(row=1, column=1)
+
+userInputSystem = IntVar()
+requestMeasurementSystem = "imperial"
+Radiobutton(root, text='Imperial', variable=userInputSystem, value=1).grid(row=2,column=1)
+Radiobutton(root,text='Metric', variable=userInputSystem, value=2).grid(row=2,column=2)
+if userInputSystem == 1:
     requestMeasurementSystem = "imperial"
-elif userInputSystem == "M":
+elif userInputSystem == 2:
     requestMeasurementSystem = "metric"
 
-getWeatherData = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={userInputCity}&units={requestMeasurementSystem}&APPID={apiKey}")
+print(userInputCity)
+print(userInputSystem)
 
-print(getWeatherData.json())
+goButton=Button(root, text="OK", width = 5, command=readyPressed).grid(row=3)
 
-weather = getWeatherData.json()['weather'][0]['main']
-temperature = round(getWeatherData.json()['main']['temp'], 2)
-longitude = round(getWeatherData.json()['coord']['lon'], 5)
-latitude = round(getWeatherData.json()['coord']['lat'], 5)
-sunrise = int(getWeatherData.json()['sys']['sunrise'])
-sunset = int(getWeatherData.json()['sys']['sunset'])
-timezone = int(getWeatherData.json()['timezone'])
-sunriseAdjusted = sunrise+timezone
-sunsetAdjusted = sunset+timezone
+if(ready==1):
+    getWeatherData = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={userInputCity}&units={requestMeasurementSystem}&APPID={apiKey}")
+    print(getWeatherData.json())
 
-print(f"Weather for {userInputCity}:")
-print(f"{longitude}, {latitude}")
-print(f"Condition: {weather}")
-print(f"Temperature: {temperature}")
-print(datetime.utcfromtimestamp(sunriseAdjusted).strftime('%H:%M'))
-print(datetime.utcfromtimestamp(sunsetAdjusted).strftime('%H:%M'))
+
+    weather = getWeatherData.json()['weather'][0]['main']
+    temperature = round(getWeatherData.json()['main']['temp'], 2)
+    longitude = round(getWeatherData.json()['coord']['lon'], 5)
+    latitude = round(getWeatherData.json()['coord']['lat'], 5)
+    sunrise = int(getWeatherData.json()['sys']['sunrise'])
+    sunset = int(getWeatherData.json()['sys']['sunset'])
+    timezone = int(getWeatherData.json()['timezone'])
+    sunriseAdjusted = sunrise+timezone
+    sunsetAdjusted = sunset+timezone
+
+    Label(root, text=f'Weather for {userInputCity} ({longitude}, {latitude}):').grid(row=4,column=1)
+    Label(root, text=f'Condition: {weather}').grid(row=4,column=2)
+    Label(root, text=f'Temperature: {temperature}').grid(row=5,column=1)
+    Label(root, text=f'Sunrise: {sunriseAdjusted}').grid(row=6,column=1)
+    Label(root, text=f'Sunset: {sunsetAdjusted}').grid(row=6,column=2)
+mainloop()
+
+
+
+
+
